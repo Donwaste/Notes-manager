e7ab78c20df095b1a077488f71620c5798cd6f0e
Donwaste <164152629+Donwaste@users.noreply.github.com>
2024-07-09 15:23:34 +0300

    Update exp.js

--- exp.js
+++ exp.js
@@ -7,13 +7,44 @@ createBtn.onclick = function () {
         return;
     }
 
-    listElement.innerHTML += `
-        <li class="list-group-item d-flex justify-content-between align-items-center">
-            <span>${inputElement.value}</span>
-            <span>
-                <span class="btn btn-small btn-success">&check;</span>
-                <span class="btn btn-small btn-danger">&times;</span>
-            </span>
-        </li>
+    const listItem = document.createElement('li');
+    listItem.className = 'list-group-item d-flex justify-content-between align-items-center';
+    listItem.innerHTML = `
+        <span>${inputElement.value}</span>
+        <span>
+            <span class="btn btn-small btn-success">&check;</span>
+            <span class="btn btn-small btn-danger">&times;</span>
+        </span>
     `;
+
+    const checkBtn = listItem.querySelector('.btn-success');
+    const deleteBtn = listItem.querySelector('.btn-danger');
+
+    checkBtn.onclick = function () {
+        const textElement = listItem.querySelector('span:first-child');
+        textElement.style.textDecoration = textElement.style.textDecoration === 'line-through' ? 'none' : 'line-through';
+        checkBtn.style.backgroundColor = checkBtn.style.backgroundColor === 'yellow' ? '#28a745' : 'yellow';
+    };
+
+    deleteBtn.onclick = function () {
+        listElement.removeChild(listItem);
+        checkEmptyList();
+    };
+
+    listElement.appendChild(listItem);
+    inputElement.value = '';
+    checkEmptyList();
 };
+
+function checkEmptyList() {
+    if (listElement.children.length === 0) {
+        listElement.innerHTML = '<li class="list-group-item">Здесь пустовато...</li>';
+    } else {
+        const emptyMessage = listElement.querySelector('.list-group-item');
+        if (emptyMessage && emptyMessage.innerText === 'Здесь пустовато...') {
+            listElement.removeChild(emptyMessage);
+        }
+    }
+}
+
+checkEmptyList();
